810885599062076a3147223ebe3e0c000de421ea
gyx <kgzforlinux@gmail.com>
2023-07-07 19:38:43 +0800

    shlab solution

---
+++ shlab-handout/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+make $101
+make $102
+make $103
+make $104
+make $105
+make $106
+make $107
+make $108
+make $109
+make $110
+make $111
+make $112
+make $113
+make $114
+make $115
+make $116
